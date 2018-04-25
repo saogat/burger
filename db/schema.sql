47a1c02bfe3d77b946926a5523330f6cffc273ea
Saogat Rab <saogat.rab@icloud.com>
2018-04-24 23:13:00 -0400

    initial

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+## Schema
+CREATE DATABASE burgers_db;
+USE burgers_db;
+CREATE TABLE burgers(
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(30),
+    devoured boolean,
+    PRIMARY KEY (id)
+)
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT into burgers SET burger_name = 'Quarter pounder', devoured = false;
+INSERT into burgers SET burger_name = 'Mushroom quarter pounder', devoured = false;
+INSERT into burgers SET burger_name = 'Spicy half pounder', devoured = false;
+
