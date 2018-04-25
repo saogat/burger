55f50ec4c3bfc9a063f5dd710842b63fa23b5467
Saogat Rab <saogat.rab@icloud.com>
2018-04-25 17:30:17 -0400

    fix to connection

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,8 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 CREATE TABLE burgers(
-    id int NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT NOT NULL,
     burger_name varchar(30),
     devoured boolean,
-    PRIMARY KEY (id)
+    PRIMARY KEY(id)
 )
